069b6823262ba3a7ee851ca4b22eb1cd1b63378d
Val Araújo <val.spminas@gmail.com>
2020-05-14 16:00:10 -0300

    Parametrização testes

---
+++ libpythonpro/spam/enviador_de_email.py
@@ -0,0 +1,3 @@
+class Enviador:
+    def enviar(self, remetente: object, destinatario: object, assunto: object, corpo: object) -> object:
+       return remetente
---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,18 @@
+from libpythonpro.spam.enviador_de_email import Enviador
+import pytest
+
+
+def test_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+@pytest.mark.parametrize("destinatario",["renzo@python.pro.br","foo@bar.com.br"])
+def test_remetente(destinatario):
+    enviador = Enviador()
+    resultado = enviador.enviar(destinatario,
+                                "luciano@python.pro.br",
+                                "Cursos python pro",
+                                "Primeira turma Guido Von Rossum aberta."
+                                )
+
+    assert destinatario in resultado
