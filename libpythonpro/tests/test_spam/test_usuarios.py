eda9b05c337bb85b50160d2f785765394d65d413
Val Araújo <val.spminas@gmail.com>
2020-05-25 08:05:12 -0300

    Implementação de Setup e tear down com fixture

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,25 +1,18 @@
-from libpythonpro.spam.bd import Conexao
 from libpythonpro.spam.modelos import Usuario
 
 
-def test_salvar_usuario():
-    conexao= Conexao()
-    sessao=conexao.gerar_sessao()
-    usuario= Usuario(nome='Renzo')
+def test_salvar_usuario(sessao):
+    usuario = Usuario(nome='Renzo')
     sessao.salvar(usuario)
-    assert isinstance(usuario.id,int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
+    assert isinstance(usuario.id, int)
 
 
-def test_listar_usuario():
-    conexao = Conexao()
-    sessao=conexao.gerar_sessao()
-    usuarios =[Usuario(nome='Renzo'), Usuario(nome='Luciano')]
+
+
+def test_listar_usuario(sessao):
+    usuarios = [Usuario(nome='Renzo'), Usuario(nome='Luciano')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
+
+
